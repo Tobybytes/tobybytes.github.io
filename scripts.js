fe712e0b4c068080fe3ed4902d238d56a931f2d9
Tobybytes <tcowton@gmail.com>
2022-11-19 22:44:28 +0000

    v.1.5 Attempted to add matrix codefall

--- scripts.js
+++ scripts.js
@@ -5,10 +5,24 @@ let i = 0;
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('form').onsubmit = function(e) {
         e.preventDefault();
-        if (document.querySelector('#number').value === answers[i]) {
+        if (document.querySelector('#number').value === answers[i] AND i < 5) {
             document.getElementById("message").innerHTML = clues[i];
             i++;
         }
+        else if (i == 5)
+            var canvas = document.createElement('canvas');
+            canvas.id = "c";
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            canvas.style.position = "absolute";
+            
+            var body = document.getElementsByTagName("body")[0];
+            body.appendChild(canvas);
+
+            setTimeout(function(){
+                body.removeChild(canvas);
+            }, 10000)
+
         else {
             document.getElementById("message").innerHTML = "Ha ha! Wrong number. My secret is safe.";
         };
