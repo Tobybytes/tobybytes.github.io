39086026b87dd46d0b512e3cc69c83c1cf283742
Tobybytes <tcowton@gmail.com>
2022-11-20 08:10:34 +0000

    v.1.6.3. Another attempt to fix the script

--- scripts.js
+++ scripts.js
@@ -4,42 +4,47 @@ let i = 0;
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = function(e) {
-        e.preventDefault();
-        if (document.querySelector('#number').value === answers[i]) {
-            document.getElementById("message").innerHTML = clues[i];
-            i++;
-            if (i == 6){
-                let body = document.getElementsByTagName("body")[0];
-                let div = document.getElementsByTagName("div")[0];
-                let canvas = document.createElement('canvas');
-                let ansdiv = document.createElement('div');
-                let ansp = document.createElement('p');
-                div.remove(div);             
-                canvas.id = "c";
-                canvas.width = window.innerWidth;
-                canvas.height = window.innerHeight;
-                canvas.style.position = "absolute";
-                body.appendChild(canvas);
-                matrix();
+    document.querySelector('form').onsubmit = function() {
+        while (i < 6)
+            if (document.querySelector('#number').value === answers[i]) {
+                document.getElementById("message").innerHTML = clues[i];
+                i++;
+                return false
+                }
+            else {
+                document.getElementById("message").innerHTML = "Ha ha! Wrong number. My secret is safe.";
+                return false
+            };
 
-                setTimeout(function(){
-                    ansdiv.className = "w3-display-middle";
-                    ansp.style.color = "#0F0";
-                    ansdiv.style.backgroundColor = "black";
-                    ansdiv.width = "25%";
-                    ansdiv.height = "15%";
-                    body.appendChild(ansdiv);
-                    body.appendChild(ansp);
-                }, 5000)
+        if (i > 5) {
+            let body = document.getElementsByTagName("body")[0];
+            let div = document.getElementsByTagName("div")[0];
+            let canvas = document.createElement('canvas');
+            let ansdiv = document.createElement('div');
+            let ansp = document.createElement('p');
+            div.remove(div);             
+            canvas.id = "c";
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            canvas.style.position = "absolute";
+            body.appendChild(canvas);
+            matrix();
 
-                setTimeout(function(){
-                    ansp.innerHTML = 'Paw Patrol DVD'
-                }, 200)
-            }
+            setTimeout(function(){
+                ansdiv.className = "w3-display-middle";
+                ansp.style.color = "#0F0";
+                ansdiv.style.backgroundColor = "black";
+                ansdiv.width = "25%";
+                ansdiv.height = "15%";
+                body.appendChild(ansdiv);
+                body.appendChild(ansp);
+            }, 5000)
+
+            setTimeout(function(){
+                ansp.innerHTML = 'Paw Patrol DVD'
+            }, 200)
+
+            return false
         }
-        else {
-            document.getElementById("message").innerHTML = "Ha ha! Wrong number. My secret is safe.";
-        };
     };
 });
