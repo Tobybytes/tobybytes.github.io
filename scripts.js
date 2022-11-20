f146f2977a97f619c93c3e616e086435e7a3c4a9
Tobybytes <tcowton@gmail.com>
2022-11-20 07:59:39 +0000

    v1.6.2. Defined matrix function and redefined manner of removing html elements then adding back in

--- scripts.js
+++ scripts.js
@@ -1,28 +1,43 @@
 let answers = ['43', '28', '12', '35', '2', '29'];
-let clues = ['First clue: ', 'Second clue: ', 'Third clue: ', 'Fourth clue: ', 'Fifth clue: ', 'Treat vouchers are hiding in the ...']
+let clues = ['First clue: ', 'Second clue: ', 'Third clue: ', 'Fourth clue: ', 'Fifth clue: ', 'Nooooooo!']
 let i = 0;
+
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('form').onsubmit = function(e) {
         e.preventDefault();
-        if (document.querySelector('#number').value === answers[i] && i < 6) {
+        if (document.querySelector('#number').value === answers[i]) {
             document.getElementById("message").innerHTML = clues[i];
             i++;
+            if (i == 6){
+                let body = document.getElementsByTagName("body")[0];
+                let div = document.getElementsByTagName("div")[0];
+                let canvas = document.createElement('canvas');
+                let ansdiv = document.createElement('div');
+                let ansp = document.createElement('p');
+                div.remove(div);             
+                canvas.id = "c";
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+                canvas.style.position = "absolute";
+                body.appendChild(canvas);
+                matrix();
+
+                setTimeout(function(){
+                    ansdiv.className = "w3-display-middle";
+                    ansp.style.color = "#0F0";
+                    ansdiv.style.backgroundColor = "black";
+                    ansdiv.width = "25%";
+                    ansdiv.height = "15%";
+                    body.appendChild(ansdiv);
+                    body.appendChild(ansp);
+                }, 5000)
+
+                setTimeout(function(){
+                    ansp.innerHTML = 'Paw Patrol DVD'
+                }, 200)
+            }
         }
-        else if (i == 6)
-            var canvas = document.createElement('canvas');
-            canvas.id = "c";
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-            canvas.style.position = "absolute";
-            
-            var body = document.getElementsByTagName("body")[0];
-            body.appendChild(canvas);
-
-            setTimeout(function(){
-                body.removeChild(canvas);
-            }, 10000)
-
         else {
             document.getElementById("message").innerHTML = "Ha ha! Wrong number. My secret is safe.";
         };
